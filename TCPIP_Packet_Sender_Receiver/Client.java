b9685e39f3ffede691a502e7cd1675bffa148e16
Soumyadeep <mukherjeesoumyadeep28@gmail.com>
2022-06-12 09:46:05 +0530

    Fixed errors

--- TCPIP_Packet_Sender_Receiver/Client.java
+++ TCPIP_Packet_Sender_Receiver/Client.java
@@ -1,19 +1,20 @@
+package TCPIP_Packet_Sender_Receiver;
+
 import java.io.*;
 import java.net.*;
-public class Client
-{
-    public static void main(String args[]) throws Exception
-    {
-        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-        System.out.println("Enter the Message");
-        String message=br.readLine();
 
-        Socket s=new Socket("LocalHost",8000);
-        DataOutputStream dos=new DataOutputStream(s.getOutputStream());
-        dos.writeBytes(message+"\n");
-        
-        BufferedReader br1=new BufferedReader(new InputStreamReader(s.getInputStream()));
-        String ack=br1.readLine();
+public class Client {
+    public static void main(String args[]) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Enter the Message");
+        String message = br.readLine();
+
+        Socket s = new Socket("LocalHost", 8000);
+        DataOutputStream dos = new DataOutputStream(s.getOutputStream());
+        dos.writeBytes(message + "\n");
+
+        BufferedReader br1 = new BufferedReader(new InputStreamReader(s.getInputStream()));
+        String ack = br1.readLine();
         System.out.println(ack);
 
         s.close();
--- TCPIP_Packet_Sender_Receiver/Server.java
+++ TCPIP_Packet_Sender_Receiver/Server.java
@@ -1,20 +1,20 @@
+package TCPIP_Packet_Sender_Receiver;
+
 import java.io.*;
 import java.net.*;
-public class Server
-{
-    public static void main(String ars[]) throws Exception
-    {
-        ServerSocket ss=new ServerSocket(8000);
-        while(true)
-        {
-            Socket s=ss.accept();
-            BufferedReader br=new BufferedReader(new InputStreamReader(s.getInputStream()));
-            String message=br.readLine();
+
+public class Server {
+    public static void main(String ars[]) throws Exception {
+        ServerSocket ss = new ServerSocket(8000);
+        while (true) {
+            Socket s = ss.accept();
+            BufferedReader br = new BufferedReader(new InputStreamReader(s.getInputStream()));
+            String message = br.readLine();
             System.out.println(message);
 
-            String ack="Acknowledgement";
-            DataOutputStream dos=new DataOutputStream(s.getOutputStream());
-            dos.writeBytes(ack+"\n");
+            String ack = "Acknowledgement";
+            DataOutputStream dos = new DataOutputStream(s.getOutputStream());
+            dos.writeBytes(ack + "\n");
 
             s.close();
         }
